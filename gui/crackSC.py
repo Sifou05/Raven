b06290899e76075c164bd2d2dc34597a91d7e4f0
Sifou05 <sifakakis05george@gmail.com>
2026-08-06 00:51:12 +0300

    Back button and some other bug changes

--- gui/crackSC.py
+++ gui/crackSC.py
@@ -2,15 +2,22 @@ import tkinter as tk
 from tkinter import filedialog
 
 class CrackScreen(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, on_goBack):
         super().__init__(parent, bg="#240f52")
         self.parent = parent
+        self.on_goBack = on_goBack
         self.create_widget()
 
     def create_widget(self):
         self.Browse_button = tk.Button(self, text="Browse Hash/File", command=self.browse_file)
         self.Browse_button.place(relx=0.5,rely=0.4, anchor="n")
         self.Browse_button.lift()
+
+        self.back_icon = tk.PhotoImage(file=r"assets/back-to-home.png")
+        self.Back_button = tk.Button(self, image=self.back_icon, command=self.on_goBack)
+        self.Back_button.place(relx=0.03, rely=0.010, anchor="n")
+        self.Back_button.lift()
+
 
 
     def browse_file(self):
--- main.py
+++ main.py
@@ -3,15 +3,28 @@ import tkinter as tk
 from gui.homeSC import HomeScreen
 from gui.crackSC import CrackScreen
 
+crack_page = None
+home_page = None
 root = tk.Tk()
 root.geometry("800x600") #Set Window Resolution
 root.title("Raven") #Set Title
 root.configure(bg='#240f52') #Set BG color
 
+def show_home_screen():
+    global crack_page, home_page
+    crack_page.destroy()
+    home_page = HomeScreen(root, show_crack_screen)
+    home_page.pack(fill="both", expand=True)
+
 def show_crack_screen():
+    global crack_page,home_page
     home_page.destroy()
-    crack_page = CrackScreen(root)
+    crack_page = CrackScreen(root, show_home_screen)
     crack_page.pack(fill="both", expand=True)
+
+
+
+
 
 
 home_page = HomeScreen(root, show_crack_screen)
